c212a9d6a582152cb1f3015182b034aeadfc9772
ip-130 <85837397+ip-130@users.noreply.github.com>
2021-07-13 16:09:47 +0300

    Initial commit

---
+++ app/src/main/java/com/google/homework_8/MainActivity.java
@@ -0,0 +1,30 @@
+package com.google.homework_8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь", "Анна", "Денис", "Андрей" };
+
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // находим список
+        ListView lvMain = (ListView) findViewById(R.id.lvMain);
+
+        // создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.list_item, names);
+
+        // присваиваем адаптер списку
+        lvMain.setAdapter(adapter);
+
+    }
+}
